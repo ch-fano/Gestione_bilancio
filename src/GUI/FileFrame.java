ff62750a9493d8be461e21e8eae99b4513eee4f4
Christofer <christofer.fano8@gmail.com>
2023-05-16 19:33:13 +0200

    Prima bozza

--- src/GUI/FileFrame.java
+++ src/GUI/FileFrame.java
@@ -3,37 +3,21 @@ package GUI;
 import javax.swing.*;
 import java.awt.*;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
 public class FileFrame  extends JFrame {
-
-    private JTextField nomefile;
-
-    private JRadioButton salva, carica;
-    private JButton ok;
-
     public FileFrame(String titolo) {
         super(titolo);
 
-        JPanel np = new JPanel();
-        nomefile = new JTextField(20);
-        np.add(new JLabel("Nome file"));
-        np.add(nomefile);
+        JFileChooser chooser = new JFileChooser();
 
-        JPanel cp = new JPanel();
-        salva = new JRadioButton("Salva");
-        carica = new JRadioButton("Carica");
+        //chooser.showOpenDialog(null);
 
-        ButtonGroup grp = new ButtonGroup();
-        grp.add(salva);
-        grp.add(carica);
-        cp.add(salva);
-        cp.add(carica);
-
-        JPanel sp = new JPanel();
-        ok = new JButton("OK");
-        sp.add(ok);
-
-        add(np, BorderLayout.NORTH);
-        add(cp, BorderLayout.CENTER);
-        add(sp, BorderLayout.SOUTH);
+        chooser.showDialog(null, "Seleziona");
+        File file = chooser.getSelectedFile();
+        String filename = file.getName();
+        System.out.println("You have selected: " + filename);
     }
 }
--- src/GUI/MainFrame.java
+++ src/GUI/MainFrame.java
@@ -6,9 +6,9 @@ import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame implements ActionListener {
 
-    private JMenu gestione, memoria, esporta, stampa;
-    private String testo_menu[] = {"Gestione bilancio","Salva o carica bilancio", "Esporta bilancio","Stampa bilancio"};
-    private String testo_voci[] = {"Visualizza", "Ricerca", "Salva bilancio", "Carica bilancio", "Formato CSV", "Formato testo", "Formato Excel", "Inizia stampa"};
+    private JMenu salva, carica, esporta, stampa;
+    private String testo_menu[] = {"Salva bilancio","Carica bilancio", "Esporta bilancio","Stampa bilancio"};
+    private String testo_voci[] = {"Salva bilancio", "Carica bilancio", "Formato CSV", "Formato testo", "Formato Excel", "Inizia stampa"};
     private GestPanel gpanel = new GestPanel();
 
     public MainFrame(String titolo) {
@@ -16,50 +16,45 @@ public class MainFrame extends JFrame implements ActionListener {
 
         JMenuBar mb = new JMenuBar();
 
-        gestione = new JMenu(testo_menu[0]);
-        memoria = new JMenu(testo_menu[1]);
+        salva = new JMenu(testo_menu[0]);
+        carica = new JMenu(testo_menu[1]);
         esporta = new JMenu(testo_menu[2]);
         stampa = new JMenu(testo_menu[3]);
 
-        JMenuItem g1 = new JMenuItem(testo_voci[0]);
-        JMenuItem g2 = new JMenuItem(testo_voci[1]);
-        JMenuItem m1 = new JMenuItem(testo_voci[2]);
-        JMenuItem m2 = new JMenuItem(testo_voci[3]);
-        JMenuItem e1 = new JMenuItem(testo_voci[4]);
-        JMenuItem e2 = new JMenuItem(testo_voci[5]);
-        JMenuItem e3 = new JMenuItem(testo_voci[6]);
-        JMenuItem s = new JMenuItem(testo_voci[7]);
+        JMenuItem s = new JMenuItem(testo_voci[0]);
+        JMenuItem c = new JMenuItem(testo_voci[1]);
+        JMenuItem e1 = new JMenuItem(testo_voci[2]);
+        JMenuItem e2 = new JMenuItem(testo_voci[3]);
+        JMenuItem e3 = new JMenuItem(testo_voci[4]);
+        JMenuItem st = new JMenuItem(testo_voci[5]);
 
-        gestione.add(g1);
-        gestione.add(g2);
+        salva.add(s);
 
-        memoria.add(m1);
-        memoria.add(m2);
+        carica.add(c);
 
         esporta.add(e1);
         esporta.add(e2);
         esporta.add(e3);
 
-        stampa.add(s);
+        stampa.add(st);
 
-        g1.addActionListener(this);
-        g2.addActionListener(this);
-        m1.addActionListener(this);
-        m2.addActionListener(this);
+        s.addActionListener(this);
+        c.addActionListener(this);
         e1.addActionListener(this);
         e2.addActionListener(this);
         e3.addActionListener(this);
-        s.addActionListener(this);
+        st.addActionListener(this);
 
-        mb.add(gestione);
-        mb.add(memoria);
+        mb.add(salva);
+        mb.add(carica);
         mb.add(esporta);
         mb.add(stampa);
 
         setJMenuBar(mb);
 
         add(gpanel);
-        gpanel.setVisible(false);
+        gpanel.setVisible(true);
+        pack();
     }
 
     @Override
@@ -67,23 +62,23 @@ public class MainFrame extends JFrame implements ActionListener {
         String button = e.getActionCommand();
 
         if (button.equals(testo_voci[0])){
-            gpanel.setVisible(true);
-            pack();
-        }
-        if (button.equals(testo_voci[1])){
-
-        }
-        if (button.equals(testo_voci[2]) || button.equals(testo_voci[3])){
             FileFrame ff = new FileFrame(button);
 
             ff.pack();
             ff.setLocationRelativeTo(null);
             ff.setVisible(true);
         }
-        if (button.equals(testo_voci[4]) || button.equals(testo_voci[5]) || button.equals(testo_voci[6])){
+        if (button.equals(testo_voci[1])){
+            FileFrame ff = new FileFrame(button);
+
+            ff.pack();
+            ff.setLocationRelativeTo(null);
+            ff.setVisible(true);
+        }
+        if (button.equals(testo_voci[2]) || button.equals(testo_voci[3]) || button.equals(testo_voci[4])){
 
         }
-        if (button.equals(testo_voci[7])){
+        if (button.equals(testo_voci[5])){
 
         }
     }
